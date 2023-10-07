a8861cbacab1f1f6442d193de91207102f0eac53
fightingsou <hirawatasou@gmail.com>
2023-10-08 01:59:04 +0900

    :recycle: Update container responsiveness
    The styles for the 'Container.svelte' component have been updated to enhance its responsiveness. Specifically, the widths for certain media query breakpoints have been adjusted. Previously, there were set widths and max-widths defined for the container at certain breakpoints. These have been simplified to a width of 100% at both the 1200px and 768px breakpoints.
    
    This change aims to simplify the container styles, making it easier to maintain and ensuring a more consistent experience across various screen sizes.

--- src/lib/components/Container.svelte
+++ src/lib/components/Container.svelte
@@ -8,15 +8,14 @@
 
   @media (max-width: 1200px) {
     .container {
-      width: 500px;
-      max-width: 960px;
+      width: 100%;
       padding: 0 1rem;
     }
   }
 
   @media (max-width: 768px) {
     .container {
-      max-width: 540px;
+      width: 100%;
       padding: 0 0.5rem;
     }
   }
